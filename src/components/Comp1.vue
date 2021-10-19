e2170d9d66cc7c495ebe8d307c828aecbb302a6e
Alex <Alexsedanosanchez@gmail.com>
2021-10-19 16:05:28 +0200

    void

---
+++ src/components/Comp1.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="comp1">
+    <h1>{{ msg }}</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comp1',
+  props: {
+    msg: String
+  },
+  data: function() {
+  return {
+    message: 'Some Message'
+  };
+}
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    
+    <Comp1 msg="Dato"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Comp1 from '@/components/Comp1.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Comp1
+  }
+}
+</script>
